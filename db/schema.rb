330272b1456e00c422819e7ea9e1588bf3d3ab13
Matteo <pierromatteo@gmail.com>
2015-08-27 11:19:50 +0200

    remove post db schema

--- db/schema.rb
+++ db/schema.rb
@@ -22,11 +22,4 @@ ActiveRecord::Schema.define(version: 20150826085453) do
     t.datetime "updated_at",                           null: false
   end
 
-  create_table "posts", force: :cascade do |t|
-    t.string   "title"
-    t.string   "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
 end
