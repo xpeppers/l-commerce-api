57c3474a8f83f533f7583c717c25d85403d9fff8
JoeBew42 <joebew42@gmail.com>
2015-08-28 17:08:23 +0200

    refactor users controller

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,10 +1,14 @@
 module Api
   class UsersController < ApplicationController
     def create
-      user = User.new(email: params[:email], password: params[:password])
-      if user.save
-	render json: user, status: :created, location: api_user_path(user)
-      end
+      user = User.create(user_params)
+      render json: user, status: :created, location: api_user_path(user)
+    end
+
+    private
+
+    def user_params
+      params.permit(:email, :password)
     end
   end
 end
