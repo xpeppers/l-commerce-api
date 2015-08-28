06accfa74ffb08ae9da720cece9c3ac11ba848dd
JoeBew42 <joebew42@gmail.com>
2015-08-28 11:09:04 +0200

    fix indentation

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,10 +1,10 @@
 module Api
-	class UsersController < ApplicationController
-		def create
-			user = User.new(email: params[:email], password: params[:password])
-			if user.save
-				render json: user, status: :created, location: api_user_path(user) 
-			end	
-		end
-	end
-end 
+  class UsersController < ApplicationController
+    def create
+      user = User.new(email: params[:email], password: params[:password])
+      if user.save
+	render json: user, status: :created, location: api_user_path(user)
+      end
+    end
+  end
+end
--- spec/controllers/api/users_controller_spec.rb
+++ spec/controllers/api/users_controller_spec.rb
@@ -2,22 +2,22 @@ require 'rails_helper'
 
 describe Api::UsersController, type: :controller do
 
-	describe 'POST #create' do
+  describe 'POST #create' do
 
-		it 'creates a new user' do
-			expect{
-				post :create, email: 'email@address.com', password: 'email@address.com'
-			}.to change(User, :count).by(1)
-		end
+    it 'creates a new user' do
+      expect{
+	post :create, email: 'email@address.com', password: 'email@address.com'
+      }.to change(User, :count).by(1)
+    end
 
-		it 'responds with new user details' do
-			post :create, email: 'email@address.com', password: 'email@address.com'
+    it 'responds with new user details' do
+      post :create, email: 'email@address.com', password: 'email@address.com'
 
-			expect(response).to have_http_status(:created)
-			expect(json).to eq({'email' => 'email@address.com'})
-			expect(response.header['Location']).to eq(api_user_path(User.last))
-		end
+      expect(response).to have_http_status(:created)
+      expect(json).to eq({'email' => 'email@address.com'})
+      expect(response.header['Location']).to eq(api_user_path(User.last))
+    end
 
-	end
+  end
 
 end
