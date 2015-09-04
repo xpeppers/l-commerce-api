f7060d945053e0bc11dfb2e63ab82dbe535b17d8
JoeBew42 <joebew42@gmail.com>
2015-09-04 10:40:06 +0200

    add environment variables for provision script

--- script/provision.sh
+++ script/provision.sh
@@ -1,15 +1,17 @@
 #!/bin/sh -e
 
+[ -z $HOST ] && HOST='192.168.33.100'
+[ -z $HOST_USER ] && HOST_USER='vagrant'
+[ -z $RUN_LIST ] && RUN_LIST='linking-commerce-api::default'
+[ -z $KNIFE_OPTS ] && KNIFE_OPTS=''
+
 berks vendor .berks-cookbooks
-# knife zero bootstrap $HOST -c ./knife.rb -x $HOST_USER -r $RUN_LIST -N $HOST --sudo $OPTIONS
 
-# knife zero bootstrap 192.168.33.100 -c ./knife.rb -x vagrant -r "linking-commerce-api::default" -N 192.168.33.100 --sudo
-
-knife zero bootstrap 192.168.33.100 \
-  --node-name 192.168.33.100 \
+knife zero bootstrap $HOST \
+  --node-name $HOST \
   --no-host-key-verify \
   --config knife.rb \
-  --ssh-user vagrant \
-  --run-list "linking-commerce-api::default" \
-  --sudo
-
+  --ssh-user $HOST_USER \
+  --run-list $RUN_LIST \
+  --sudo \
+  $KNIFE_OPTS
