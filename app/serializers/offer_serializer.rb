ff5cc549265e9817bec69d24831d88f57e7a0a7a
Matteo <xp@XPepperss-MacBook-Pro.local>
2015-09-10 16:47:43 +0200

    move offer list serializer in a file

--- app/serializers/offer_serializer.rb
+++ app/serializers/offer_serializer.rb
@@ -1,15 +1,3 @@
-class OfferListSerializer < ActiveModel::Serializer
-  attributes :id, :title, :description, :original_price, :price, :image_url
-
-  def price
-    '%.2f' % object.price
-  end
-
-  def original_price
-    '%.2f' % object.original_price unless object.original_price.nil?
-  end
-end
-
 class OfferSerializer < OfferListSerializer
   attributes :merchant
 
