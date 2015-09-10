4ec6e1434aebcd9f9f0cbd61003ca236a5805e98
Matteo <xp@XPepperss-MacBook-Pro.local>
2015-09-10 14:50:09 +0200

    add test for payment helper

---
+++ spec/lib/payment_helper_spec.rb
@@ -0,0 +1,55 @@
+require 'rails_helper'
+include PayPal::SDK
+
+describe PaymentHelper, type: :lib do
+
+  it 'captures an authorized paypal payment' do
+      result = PaymentHelper::capture_authorized_payment(paypal_payment_id)
+      expect(result).to be true
+  end
+end
+
+def paypal_payment_id
+  payload = {
+    :intent => "authorize",
+    :payer => {
+      :payment_method => "credit_card",
+      :funding_instruments => [
+        {
+          :credit_card => {
+            :type => "visa",
+            :number => "4020027906955647",
+            :expire_month => "07", :expire_year => "2020",
+            :cvv2 => "598",
+            :first_name => "JOE", :last_name => "FOO",
+            :billing_address => {
+              :line1 => "52 N Main ST",
+              :city => "Johnstown",
+              :state => "OH",
+              :postal_code => "43210",
+              :country_code => "US"
+            }
+          }
+        }
+      ]
+    },
+    :transactions => [
+      {
+        :amount => {
+          :total => "7.47",
+          :currency => "USD",
+          :details => {
+            :subtotal => "7.41",
+            :tax => "0.03",
+            :shipping => "0.03"
+          }
+        },
+        :description => "This is the payment transaction description."
+      }
+    ]
+  }
+
+  payment = REST::Payment.new(payload)
+  payment.create
+  payment.id
+end
--- spec/models/payment_spec.rb
+++ spec/models/payment_spec.rb
@@ -1,5 +1,4 @@
 require 'rails_helper'
-include PayPal::SDK
 
 describe Payment, type: :model do
   it { should belong_to(:order) }
@@ -9,58 +8,15 @@ describe Payment, type: :model do
     let(:user) { create(:user) }
     let(:offer) { create(:offer) }
     let(:order) { create(:order, user: user, offers: [offer]) }
-    let(:payment) { create(:payment, paypal_payment_token: paypal_payment_token, order: order) }
+    let(:payment) { create(:payment, paypal_payment_token: 'ANY TOKEN', order: order) }
 
     it 'captures the payment' do
+      expect(PaymentHelper).to receive(:capture_authorized_payment).with('ANY TOKEN').and_return(true)
+
       payment.capture!
 
       expect(payment).to be_captured
     end
 
   end
-end
-
-def paypal_payment_token
-  payload = {
-    :intent => "authorize",
-    :payer => {
-      :payment_method => "credit_card",
-      :funding_instruments => [
-        {
-          :credit_card => {
-            :type => "visa",
-            :number => "4020027906955647",
-            :expire_month => "07", :expire_year => "2020",
-            :cvv2 => "598",
-            :first_name => "JOE", :last_name => "FOO",
-            :billing_address => {
-              :line1 => "52 N Main ST",
-              :city => "Johnstown",
-              :state => "OH",
-              :postal_code => "43210",
-              :country_code => "US"
-            }
-          }
-        }
-      ]
-    },
-    :transactions => [
-      {
-        :amount => {
-          :total => "7.47",
-          :currency => "USD",
-          :details => {
-            :subtotal => "7.41",
-            :tax => "0.03",
-            :shipping => "0.03"
-          }
-        },
-        :description => "This is the payment transaction description."
-      }
-    ]
-  }
-
-  payment = REST::Payment.new(payload)
-  payment.create
-  payment.id
 end
