04cb634fa10903d49fdb41d2440294ccb78765e5
JoeBew42 <joebew42@gmail.com>
2015-09-22 11:44:53 +0200

    Responds with unauthorized if invalid provider token is provided

--- spec/controllers/api/auth_controller_spec.rb
+++ spec/controllers/api/auth_controller_spec.rb
@@ -5,14 +5,17 @@ describe Api::AuthController, type: :controller do
   describe 'POST #create' do
 
     context 'with valid user' do
+      PROVIDER_TOKEN = 'A PROVIDER TOKEN'
+      PROVIDER_USER_ID = 'A PROVIDER USER ID'
+
       before do
-        create(:user, provider_user_id: 'A PROVIDER USER ID')
+        create(:user, provider_user_id: PROVIDER_USER_ID)
       end
 
       it 'creates an authentication token' do
-        expect(FacebookIdentity).to receive(:user_id_from).with('A PROVIDER TOKEN').and_return('A PROVIDER USER ID')
+        expect(FacebookIdentity).to receive(:user_id_from).with(PROVIDER_TOKEN).and_return(PROVIDER_USER_ID)
 
-        post :create, provider: 'facebook', provider_token: 'A PROVIDER TOKEN'
+        post :create, provider: 'facebook', provider_token: PROVIDER_TOKEN
 
         expect(response).to have_http_status(:created)
 
@@ -21,6 +24,18 @@ describe Api::AuthController, type: :controller do
         })
 
         expect(response.body).to be_json_eql(expected_json)
+      end
+    end
+
+    context 'with invalid user' do
+      it 'responds with unauthorized' do
+        INVALID_PROVIDER_TOKEN = 'INVALID PROVIDER TOKEN'
+
+        expect(FacebookIdentity).to receive(:user_id_from).with(INVALID_PROVIDER_TOKEN).and_return(nil)
+
+        post :create, provider: 'facebook', provider_token: INVALID_PROVIDER_TOKEN
+
+        expect(response).to have_http_status(:unauthorized)
       end
     end
 
