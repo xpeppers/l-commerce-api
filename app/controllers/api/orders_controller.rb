a2a428280d6e1635185bcbcf38ca9affc06dcb42
JoeBew42 <joebew42@gmail.com>
2015-09-22 18:00:42 +0200

    Add token validation for orders controller

--- app/controllers/api/orders_controller.rb
+++ app/controllers/api/orders_controller.rb
@@ -1,5 +1,7 @@
 module Api
   class OrdersController < ApplicationController
+    before_action :validate_user_authentication, only: [:create, :show]
+
     def create
       order = Order.create(order_params)
       render json: order, status: :created, location: api_order_path(order)
@@ -11,6 +13,16 @@ module Api
     end
 
     private
+
+    def validate_user_authentication
+      return render json: nil, status: :unauthorized if authenticated_user.nil?
+    end
+
+    def authenticated_user
+      authenticate_with_http_token do |token, options|
+        User.find_by(token: token)
+      end
+    end
 
     def order_params
       params.permit(:user_id, :offer_ids => [])
--- spec/controllers/api/orders_controller_spec.rb
+++ spec/controllers/api/orders_controller_spec.rb
@@ -3,23 +3,28 @@ require 'rails_helper'
 describe Api::OrdersController, type: :controller do
 
   describe 'POST #create' do
-    let(:user) { create(:user) }
-    let(:offer) { create(:offer) }
 
-    it 'creates a new order' do
-      expect{
+    context 'with an authenticated user' do
+      let(:user) { create(:user, token: 'ANY TOKEN') }
+      let(:offer) { create(:offer) }
+
+      before do
+        request.headers['Authorization'] = "Token token=#{user.token}"
+      end
+
+      it 'creates a new order' do
+        expect{
+          post :create, user_id: user, offer_ids: [offer]
+        }.to change(Order, :count).by(1)
+      end
+
+      it 'responds with order details' do
         post :create, user_id: user, offer_ids: [offer]
-      }.to change(Order, :count).by(1)
-    end
 
-    it 'responds with order details' do
-      post :create, user_id: user, offer_ids: [offer]
+        expect(response).to have_http_status(:created)
+        expect(response.header['Location']).to eq(api_order_path(Order.last))
 
-      expect(response).to have_http_status(:created)
-      expect(response.header['Location']).to eq(api_order_path(Order.last))
-
-      expected_json = %(
-        {
+        expected_json = %({
           "id": #{Order.last.id},
           "user_id": #{user.id},
           "offers": [
@@ -34,28 +39,58 @@ describe Api::OrdersController, type: :controller do
           ],
           "status": "pending",
           "coupon": null
-        }
-      )
+        })
 
-      expect(response.body).to be_json_eql(expected_json)
+        expect(response.body).to be_json_eql(expected_json)
+      end
+    end
+
+    context 'with an unauthenticated user' do
+      let(:user) { create(:user) }
+      let(:offer) { create(:offer) }
+
+      describe 'without token' do
+        it 'responds with unauthorized' do
+          post :create, user_id: user, offer_ids: [offer]
+
+          expect(response).to have_http_status(:unauthorized)
+        end
+      end
+
+      describe 'with invalid token' do
+        before do
+          request.headers['Authorization'] = 'Token token=INVALID_TOKEN'
+        end
+
+        it 'responds with unauthorized' do
+          post :create, user_id: user, offer_ids: [offer]
+
+          expect(response).to have_http_status(:unauthorized)
+        end
+      end
     end
   end
 
   describe 'GET #show' do
-    context "when status is 'captured'" do
+    context 'with an authenticated user' do
 
-      let(:user) { create(:user) }
-      let(:offer) { create(:offer) }
-      let(:coupon) { create(:coupon, code: 'XXX') }
-      let(:order) { create(:captured_order, user: user, offers: [offer], coupon: coupon) }
+      let(:user) { create(:user, token: 'ANY TOKEN') }
 
-      it 'responds with coupon details' do
-        get :show, id: order
+      before do
+        request.headers['Authorization'] = "Token token=#{user.token}"
+      end
 
-        expect(response).to have_http_status(:ok)
+      describe "with a 'captured' order" do
+        let(:offer) { create(:offer) }
+        let(:coupon) { create(:coupon, code: 'XXX') }
+        let(:order) { create(:captured_order, user: user, offers: [offer], coupon: coupon) }
 
-        expected_json = %(
-          {
+        it 'responds with coupon details' do
+          get :show, id: order
+
+          expect(response).to have_http_status(:ok)
+
+          expected_json = %({
             "id": #{order.id},
             "user_id": #{user.id},
             "offers": [
@@ -72,10 +107,23 @@ describe Api::OrdersController, type: :controller do
               "id": #{coupon.id},
               "code": "#{coupon.code}"
             }
-          }
-        )
+          })
 
-        expect(response.body).to be_json_eql(expected_json)
+          expect(response.body).to be_json_eql(expected_json)
+        end
+      end
+    end
+
+    context 'with an unautheticated user' do
+
+      let(:user) { create(:user) }
+      let(:offer) { create(:offer) }
+      let(:order) { create(:order, user: user, offers: [offer]) }
+
+      it 'responds with unauthorized' do
+        get :show, id: order
+
+        expect(response).to have_http_status(:unauthorized)
       end
 
     end
