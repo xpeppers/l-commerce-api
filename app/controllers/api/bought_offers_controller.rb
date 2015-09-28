ba0d5b5dbe81aa21570f15e505cc1ab74bb5442b
JoeBew42 <joebew42@gmail.com>
2015-09-28 15:39:10 +0200

    extract bought_offer_params

--- app/controllers/api/bought_offers_controller.rb
+++ app/controllers/api/bought_offers_controller.rb
@@ -6,13 +6,19 @@ module Api
     end
 
     def show
-      bought_offer = @authenticated_user.bought_offers.find_by(id: params[:id])
+      bought_offer = @authenticated_user.bought_offers.find_by(bought_offer_params)
 
       if bought_offer.nil?
         render json: nil, status: :not_found
       else
         render json: bought_offer
       end
+    end
+
+    private
+
+    def bought_offer_params
+      params.permit(:id)
     end
   end
 end
