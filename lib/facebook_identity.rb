3b6c36d8077a058ab46b3ac8f27f8133950fe7d4
BFranzcesco <franzb475@gmail.com>
2015-10-01 11:47:28 +0200

    get facebook user full name and profile picture

--- lib/facebook_identity.rb
+++ lib/facebook_identity.rb
@@ -7,4 +7,24 @@ module FacebookIdentity
     graph.debug_token(token)['data']['user_id']
   end
 
+  def self.full_name_from(user_id)
+    oauth = Facebook::OAuth.new(FACEBOOK_CONFIG['app_id'], FACEBOOK_CONFIG['app_secret'], nil)
+    graph = Facebook::API.new(oauth.get_app_access_token)
+    begin
+      graph.get_object(user_id)['name']
+    rescue
+      nil
+    end
+  end
+
+  def self.profile_picture_url_from(user_id)
+    oauth = Facebook::OAuth.new(FACEBOOK_CONFIG['app_id'], FACEBOOK_CONFIG['app_secret'], nil)
+    graph = Facebook::API.new(oauth.get_app_access_token)
+    begin
+      graph.get_picture(user_id, type: 'large')
+    rescue
+      nil
+    end
+  end
+
 end
--- spec/lib/facebook_identity_spec.rb
+++ spec/lib/facebook_identity_spec.rb
@@ -15,10 +15,35 @@ describe FacebookIdentity, type: :lib, integration: true do
     end
   end
 
+  context 'with a valid user id' do
+    it 'returns the user full name' do
+      expect(FacebookIdentity.full_name_from(valid_user_id)).to_not be_nil
+    end
+
+    it 'returns user profile picture url' do
+      expect(FacebookIdentity.profile_picture_url_from(valid_user_id)).to_not be_nil
+    end
+  end
+
+  context 'with an invalid user id' do
+    it 'returns nil as user full name' do
+      expect(FacebookIdentity.full_name_from('INVALID USER ID')).to be_nil
+    end
+
+    it 'returns nil as user profile picture url' do
+      expect(FacebookIdentity.profile_picture_url_from('INVALID USER ID')).to be_nil
+    end
+  end
+
 end
 
 
 def valid_user_access_token
   users = Facebook::TestUsers.new(app_id: FACEBOOK_CONFIG['app_id'], secret: FACEBOOK_CONFIG['app_secret'])
   users.list.first['access_token']
+end
+
+def valid_user_id
+  users = Facebook::TestUsers.new(app_id: FACEBOOK_CONFIG['app_id'], secret: FACEBOOK_CONFIG['app_secret'])
+  users.list.first['id']
 end
