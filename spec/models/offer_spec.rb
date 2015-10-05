7b87cbb6dbac3c7262db6ed6a376fbf72e8bab5a
Paolo D'Incau <paolo.dincau@xpeppers.com>
2015-10-05 15:04:38 +0200

    Adding test for #image_url

--- spec/models/offer_spec.rb
+++ spec/models/offer_spec.rb
@@ -5,4 +5,14 @@ describe Offer, type: :model do
   it { expect(subject).to have_one :image_gallery }
   it { expect(subject).to have_many(:images).through(:image_gallery) }
 
+  describe '#image_url' do
+    let(:image) { create(:image, resource: File.open("#{Rails.root}/spec/fixtures/images/image.jpg")) }
+    let(:image_gallery) { create(:image_gallery, images: [image]) }
+
+    it 'returns the full url of the first image' do
+      offer = Offer.create(image_gallery: image_gallery)
+
+      expect(offer.image_url).to eq('http://127.0.0.1:3000/uploads/image/resource/1/image.jpg')
+    end
+  end
 end
