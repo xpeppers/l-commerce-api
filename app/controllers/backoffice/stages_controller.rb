41faa521a2ae0b19756fe0cedf9afbfe249e7420
Paolo D'Incau <paolo.dincau@xpeppers.com>
2015-11-10 11:51:56 +0100

    Adding backoffice for stage

---
+++ app/controllers/backoffice/stages_controller.rb
@@ -0,0 +1,42 @@
+module Backoffice
+  class StagesController < AuthenticatedController
+    before_action :set_stage, only: [:show, :edit, :update, :destroy]
+
+    def new
+      @stage = Stage.new(path_id: params[:path_id])
+    end
+
+    def create
+      @stage = Stage.new(stage_params)
+
+      if @stage.save
+        redirect_to [:backoffice, @stage.path, @stage], notice: 'Stage was successfully created.'
+      else
+        render :new
+      end
+    end
+
+    def update
+      if @stage.update(stage_params)
+        redirect_to [:backoffice, @stage.path, @stage], notice: 'Stage was successfully updated.'
+      else
+        render :edit
+      end
+    end
+
+    def destroy
+      @stage.destroy
+      redirect_to backoffice_paths_url, notice: 'Stage was successfully destroyed.'
+    end
+
+    private
+
+    def set_stage
+      @stage = Stage.find(params[:id])
+    end
+
+    def stage_params
+      params.require(:stage).permit(:title, :description, :image_id, :merchant_id, :path_id)
+    end
+  end
+end
