6b762338d2bf76bd646d0e56b12c704065518a9c
linnal <erindajaupi@gmail.com>
2016-04-15 11:14:59 +0200

    added url for list offers, check if reservetion_price is null

--- app/serializers/offer_list_serializer.rb
+++ app/serializers/offer_list_serializer.rb
@@ -1,5 +1,7 @@
 class OfferListSerializer < ActiveModel::Serializer
-  attributes :id, :title, :description, :original_price, :price, :reservation_price, :image_url, :latitude, :longitude
+  include Rails.application.routes.url_helpers
+
+  attributes :id, :title, :description, :original_price, :price, :reservation_price, :image_url, :latitude, :longitude, :url
 
   def latitude
     object.merchant.latitude
@@ -14,10 +16,14 @@ class OfferListSerializer < ActiveModel::Serializer
   end
 
   def reservation_price
-    '%.2f' % object.reservation_price
+    '%.2f' % object.reservation_price unless object.reservation_price.nil?
   end
 
   def original_price
     '%.2f' % object.original_price unless object.original_price.nil?
+  end
+
+  def url
+      frontend_offer_url(object, host: Rails.configuration.asset_host)
   end
 end
