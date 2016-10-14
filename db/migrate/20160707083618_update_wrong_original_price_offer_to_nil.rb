84cf977c37fed4b6b4785bedfe917e5d81563d5a
linnal <erindajaupi@gmail.com>
2016-10-14 17:37:45 +0200

    removed check presenze for row_order

--- db/migrate/20160707083618_update_wrong_original_price_offer_to_nil.rb
+++ db/migrate/20160707083618_update_wrong_original_price_offer_to_nil.rb
@@ -2,7 +2,7 @@ class UpdateWrongOriginalPriceOfferToNil < ActiveRecord::Migration
   def change
     offers = Offer.all
     offers.each do |offer|
-      if offer.original_price.present? && offer.price >= offer.original_price
+      if offer.original_price.present? && offer.price.present? && offer.price >= offer.original_price
         offer.original_price = nil
         offer.save
       end
