dab64789c467e8e9951a0df8cda3d045063c32a2
linnal <erindajaupi@gmail.com>
2016-10-14 16:11:20 +0200

    remove useless comments

--- db/migrate/20160707093618_add_number_to_row_order.rb
+++ db/migrate/20160707093618_add_number_to_row_order.rb
@@ -1,13 +1,11 @@
 class AddNumberToRowOrder < ActiveRecord::Migration
   def change
-  	# Find all duplicate records and group them by a field
     offers = Offer.all.order("created_at desc")
     i = 0
-    # Iterate on each grouped item to destroy duplicate
     offers.each do |offer|
       offer.row_order = i
+      offer.save
       i += 1
-      offer.save()
     end
   end
 end
